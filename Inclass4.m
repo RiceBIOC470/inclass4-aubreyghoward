728778924c55be05d9efe361db190d5659c5a1a2
aubreyghoward <aubrey.g.howard@rice.edu>
2017-09-07 15:38:12 -0500

    EndOfClassCommit

--- Inclass4.m
+++ Inclass4.m
@@ -5,18 +5,19 @@
 %2" on its own line, then 9 lines with 7 random numbers each. 
 filename = 'randomNumbers.txt';
 fid = fopen(filename,'w');
+
 fprintf(fid,'Random Numbers 1 \n');
-ii = 1;
-while ii <10
+ii = 0;
+while ii < 10
    ii = ii+1;
    rand_Num = rand(1,13);
    str2prnt = num2str(rand_Num);
    fprintf(fid,'%s\n',str2prnt);
-end
-fprintf(fid,'Random Numbers 2 \n');
+end %%prints 13 random Numbers on 10 lines
 
-ii = 1;
-while ii <9
+fprintf(fid,'Random Numbers 2 \n');%%prints 7 random Numbers on 9 lines
+ii = 0;
+while ii < 9
    ii = ii+1;
    rand_Num = rand(1,7);
    str2prnt = num2str(rand_Num);
@@ -25,6 +26,8 @@ end
 %%
 %(b) Write code that reads only the first line of random numbers in the file and stores
 %them as numbers in an array. 
+
+
 
 % 2. Write a function that takes an array as input and returns a logical
 % variable which is true if the sum of the numbers in the array is greater
